083ce1f5b2a59354129d9a3434b372db12779bd8
Austen McDonald <austenmc@fb.com>
2017-02-01 15:32:19 -0800

    Add a single section datasource to make large tableviews easier

---
+++ Anchor/ARSingleSectionDataSource.h
@@ -0,0 +1,15 @@
+//
+//  ARSingleSectionDataSource.h
+//  Anchor
+//
+//  Created by Austen McDonald on 2/1/17.
+//  Copyright © 2017 Roger Huffstetler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <UIKit/UITableViewController.h>
+
+@interface ARSingleSectionDataSource : NSObject <UITableViewDataSource>
+
+@end
---
+++ Anchor/ARSingleSectionDataSource.m
@@ -0,0 +1,28 @@
+//
+//  ARSingleSectionDataSource.m
+//  Anchor
+//
+//  Created by Austen McDonald on 2/1/17.
+//  Copyright © 2017 Roger Huffstetler. All rights reserved.
+//
+
+#import "ARSingleSectionDataSource.h"
+
+@implementation ARSingleSectionDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+  return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+  return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  return nil;
+}
+
+@end
